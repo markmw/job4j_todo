8ca77bf7a259577f23fa03799f6ff4f3f4d1a268
markmw <dant1kkz@gmail.com>
2022-09-19 10:44:35 +0600

    Создание модели/контроллера/сервиса задачи

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,77 @@
+package ru.job4j.todo.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Task implements Serializable {
+    private int id;
+    private String description;
+    private LocalDateTime created;
+    private boolean done;
+
+    public Task() {
+    }
+
+    public Task(int id, String description, LocalDateTime created, boolean done) {
+        this.id = id;
+        this.description = description;
+        this.created = created;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{"
+                + "id=" + id
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + ", done=" + done
+                + '}';
+    }
+}
